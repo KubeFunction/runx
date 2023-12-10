5adecac76db6bec212d1d359ad7919b895222b28
liuzhenwei <dui_zhang@163.com>
2023-12-10 19:33:09 +0800

    support kill sub cmd
    Signed-off-by: liuzhenwei <dui_zhang@163.com>

--- pkg/cmd/wasm_kill.go
+++ pkg/cmd/wasm_kill.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"k8s.io/klog"
+
+	"github.com/kubefunction/runx/pkg/sandbox"
+	"github.com/kubefunction/runx/pkg/sandbox/wasm"
 )
 
 type WasmKillOption struct {
@@ -15,4 +18,15 @@ func (o *WasmKillOption) Run() error {
 		return err
 	}
 	return nil
+}
+func (o *WasmKillOption) Complete() {
+	switch o.Runtime {
+	case sandbox.WasmEdgeRuntime:
+		c := &wasm.WasmEdgeSandboxConfig{
+			Pid: o.Pid,
+		}
+		o.Sandbox = wasm.NewWasmEdgeSandbox(c)
+	default:
+		klog.Fatalf("not support the wasm runtime %s", o.Runtime)
+	}
 }
